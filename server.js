cc15f21f83fb18e2923b7291c3508473686610ee
PRUDHVI <prudvihemanth@gmail.com>
2019-06-01 15:21:10 +0530

    connected mysql

--- server.js
+++ server.js
@@ -2,11 +2,30 @@ const Hapi = require('@hapi/hapi');
 const Inert = require('@hapi/inert');
 const Vision = require('@hapi/vision');
 const HapiSwagger = require('hapi-swagger');
+const mysql = require('mysql');
 require('dotenv').config();
 
 
 const routes = require('./server/routes/apiRoutes.js');
 const logger = require('./server/utils/utils.js');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+connection.connect((error) => {
+  if (error) {
+    logger.error('error connecting mysql db');
+  }
+  connection.query('CREATE DATABASE IF NOT EXISTS bike_db', (err) => {
+    if (err) {
+      logger.error(err);
+    }
+    logger.info('Database connected');
+  });
+});
 
 const init = async () => {
   const server = Hapi.server({
@@ -31,7 +50,7 @@ const init = async () => {
     },
   ]);
 
-  await server.route(routes);
+  server.route(routes);
   await server.start();
   logger.info(`Server running on ${server.info.uri}`);
 };
